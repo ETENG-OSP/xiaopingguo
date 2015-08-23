fbae31cb7c1b34f136783777e574cfde7ef044ec
Gen Liu <gerald_721@hotmail.com>
2015-08-23 23:44:44 +0800

    init

---
+++ eteng.js
@@ -0,0 +1,31 @@
+var uploadUrl = '//127.0.0.1:3001/api/score';
+var rankUrl = '//127.0.0.1:3001/api/rank';
+
+window.eteng = {
+  uploadScore: uploadScore,
+  getRank: getRank
+};
+
+function uploadScore(score) {
+  return getOpenID().then(function(openID) {
+    return $.ajax({
+      method: 'PUT',
+      url: uploadUrl,
+      data: {
+        score: score,
+        openID: getOpenID()
+      }
+    });
+  });
+}
+
+function getRank() {
+  return getOpenID().then(function(openID) {
+    return $.ajax({
+      url: rankUrl,
+      data: {
+        openID: openID
+      }
+    });
+  });
+}
---
+++ weixin-openid.js
@@ -0,0 +1,86 @@
+var scripts = document.getElementsByTagName('script');
+var thisTag = scripts[scripts.length - 1];
+var appid = thisTag.getAttribute('data-appid');
+
+if (!appid) {
+  throw new Error('appid is not set');
+}
+
+getOpenID()['catch'](function() {
+  weixinAuth();
+});
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+    results = regex.exec(window.location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function encodeQueryString(params) {
+  return Object
+    .keys(params)
+    .map(function(key) {return key + '=' + encodeURIComponent(params[key])})
+    .join('&');
+}
+
+function weixinAuth() {
+  var url = 'https://open.weixin.qq.com/connect/oauth2/authorize';
+  var qs = encodeQueryString({
+    appid: appid,
+    'redirect_uri': window.location.href,
+    'response_type': 'code',
+    scope: 'snsapi_base'
+  }) + '#wechat_redirect';
+  window.location.assign(url + '?' + qs);
+}
+
+function getOpenID() {
+  return new Promise(function(resolve, reject) {
+    var openID = loadID();
+    if (openID) {
+      return resolve(openID);
+    }
+
+    var code = getParameterByName('code');
+    if (code) {
+      return requestToken(code).then(function(results) {
+        var openid = results.openid;
+        saveID(openid);
+        resolve(openid);
+      }, function() {
+        reject('can not retrive openid from server');
+      });
+    }
+
+    reject('not auth');
+  });
+}
+
+function requestToken(code) {
+  var url = '/api/token?code=' + code;
+
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function() {
+      if (req.status == 200) {
+        resolve(JSON.parse(req.responseText));
+      } else {
+        reject(Error(req.statusText));
+      }
+    };
+    req.onerror = function() {
+      reject(Error('Network Error'));
+    };
+    req.send();
+  });
+}
+
+function saveID(openid) {
+  localStorage['weixin-openid-' + appid] = openid;
+}
+
+function loadID() {
+  return localStorage['weixin-openid-' + appid];
+}
